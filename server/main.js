f0b83f27e2ebec261a45e63646f5e937bfe0c224
Matt Matteis <matt.matteis@gmail.com>
2017-09-30 11:14:40 -0400

    Initial commit

---
+++ server/main.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+import moment from 'moment';
+import '../imports/api/users';
+import '../imports/startup/simple-schema-configuration.js';
+
+Meteor.startup(() => {
+
+});
